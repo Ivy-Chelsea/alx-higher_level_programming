e37890df4947900d40fde12a8adb3eafee2491a8
Ivy-Chelsea <105067998+Ivy-Chelsea@users.noreply.github.com>
2022-08-31 22:08:24 +0300

    convert roman numeral to integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 def roman_to_int(roman_string):
     if not roman_string or type(roman_string) != str:
         return 0
-    roman_d = {'I': 1, 'V': 5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+    roman_d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     roman_n = 0
     for j in range(len(roman_string)):
         if j > 0 and roman_d[roman_string[j]] > roman_d[roman_string[j - 1]]:
