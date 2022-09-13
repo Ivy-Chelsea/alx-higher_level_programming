b42c6c0d90639b53830b0e43fcc021840f2a6008
Ivy-Chelsea <ivychelsea20@gmail.com>
2022-09-13 13:31:12 -0700

    task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" Class Square that defines a square based on 4-square.py """
+
+
+class Square:
+    def ___init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+                raise TypeError("size must be an integer")
+        elif value < 0:
+                raise ValueError("size must be >= 0")
+        else:
+                self.__size = value
+
+    def area(self):
+        return self.__size * self.__size
+
+    def my_print(self):
+        if self.__size == 0:
+            print('')
+        for s in range(self.__size):
+            for os in range(self.__size):
+                print('#', end='')
+            print('')
