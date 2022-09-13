e4c7ddcb73648b16902667cd47422eea29edfe4a
Ivy-Chelsea <105067998+Ivy-Chelsea@users.noreply.github.com>
2022-09-13 23:55:25 +0300

    task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -36,7 +36,7 @@ class Square:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
-            else:
+        else:
             self.__size = value
 
     @property
