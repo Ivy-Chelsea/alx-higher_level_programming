f19067ed21926237ccc015357efa18317052c379
Ivy-Chelsea <ivychelsea20@gmail.com>
2022-09-13 12:11:41 -0700

    task1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Class Square that defines a square by:(based on 0-square.py)
+    Private attribute size
+    Installation with size(no type/value verification
+    Not allowedto import any module """
+
+
+class Square:
+    """ Square class """
+    def __init__(self, size):
+        """ Initialize class """
+        self.__size = size
