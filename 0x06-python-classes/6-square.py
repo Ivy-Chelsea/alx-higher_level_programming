085e00bef931b8129c24c271954b171856057c43
Ivy-Chelsea <105067998+Ivy-Chelsea@users.noreply.github.com>
2022-09-15 02:15:55 +0300

    hot fix

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
+"""creates class Square."""
 
 
 class Square:
     """ Square class defined
-    Attributes:
-    size (int): Size of square
-    position (tuple): position of space and new lines
+        Attributes:
+            size (int): Size of square
+            position (tuple): position of space and new lines
     """
     def __init__(self, size=0, position=(0, 0)):
         """initializes
         Args:
-        size (int): size
-        position(tuple): position
-        Returns: None
+            size (int): size
+            postion(tuple): postion
+        Returns:
+            None
         """
 
         self.size = size
@@ -20,18 +22,25 @@ class Square:
 
     @property
     def size(self):
-        """ getter of size
-        Return: Size of Square
         """
+        getter of size
+        Return:
+            Size of square
+        """
+        return self.__size
 
     @size.setter
     def size(self, value):
-        """ Setter of size
-        Args: value (int): size
-        Raises: TypeError is size not int
-                ValueError if size is less than 0
-                Returns: None
-                """
+        """
+        Setter of size
+        Args:
+            value (int): size
+        Raises
+            TypeError: if size is not int
+            ValueError: size less than 0
+        Returns:
+            None
+        """
         if type(value) != int:
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -41,23 +50,42 @@ class Square:
 
     @property
     def position(self):
-        """ get position atrribute"""
+        """
+        get postion attribute
+        """
         return self.__position
 
     @position.setter
     def position(self, value):
+        """
+            setter of position
+        Args:
+            value (tuple): position of the square in 2D space
+        Returns:
+            None
+        """
         if len(value) != 2 or type(value) != tuple:
             raise TypeError("position must be a tuple of 2 positive integers")
         if type(value[0]) != int or value[0] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         if type(value[1]) != int or value[1] < 0:
-            raise TypeError("position must be a tupke of 2 positive integers")
-        swlf.__position = value
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
 
     def area(self):
+        """
+        get area
+        Return:
+            area (int)
+        """
         return self.__size ** 2
 
     def my_print(self):
+        """
+        print a square
+        Returns:
+            None
+        """
         if self.size == 0:
             print()
         else:
