baca4f73e8d4aef4d74b42e5fb68b11672109d6a
Ivy-Chelsea <105067998+Ivy-Chelsea@users.noreply.github.com>
2022-09-15 02:09:07 +0300

    fix0

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-""" Class square that defines a square by:(based on 1-square.py)
-    private instance attribute: size """
+""" creates class Square """
 
 
 class Square:
+    """ Square class"""
     def __init__(self, size=0):
         if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
-            raise ValueError("Size must be >= 0")
+            raise ValueError("size must be >= 0")
         else:
             self.__size = size
