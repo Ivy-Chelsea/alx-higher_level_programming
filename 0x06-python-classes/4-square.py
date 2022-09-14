60a00a4737f4f3349d343b089dc46a315f91dbef
Ivy-Chelsea <105067998+Ivy-Chelsea@users.noreply.github.com>
2022-09-15 02:12:19 +0300

    fix2

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-""" Class square that defines a square based on 3-square.py """
+""" creates class Square """
 
 
 class Square:
-    def ___init___(self, size=0):
+    """ Square class"""
+    def __init__(self, size=0):
         if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
@@ -11,18 +12,18 @@ class Square:
         else:
             self.__size = size
 
-        @property
-        def size(self):
-            return self.__size
+    @property
+    def size(self):
+        return self.__size
 
-        @size.setter
-        def size(self, value):
-            if type(value) != int:
-                raise TypeError("size must be an integer")
-            elif value < 0:
-                raise ValueError("size must be >= 0")
-            else:
-                self.__size = value
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
 
-            def area(sefl):
-                return self.__size * self.__size
+    def area(self):
+        return self.__size * self.__size
