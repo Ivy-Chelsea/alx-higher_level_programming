73b8fd12a0ec18ed1a2afa7f5df71769f94f1cda
Ivy-Chelsea <105067998+Ivy-Chelsea@users.noreply.github.com>
2022-09-15 02:28:42 +0300

    division

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-def safe_print_devision(a, b):
+def safe_print_division(a, b):
     try:
-        div = a / b
-    except (TypeError, ZeroDivisionError):
-        div = None
+        result = a / b
+    except ZeroDivisionError:
+        result = None
     finally:
-        print('Inside result: {}'.format(div))
-    return (div)
+        print('Inside result: {}'.format(result))
+    return result
