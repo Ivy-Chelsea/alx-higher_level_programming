1411b1ff6dc472b85fa3d68c7a4be7cd69164128
Ivy-Chelsea <105067998+Ivy-Chelsea@users.noreply.github.com>
2022-09-23 22:49:06 +0300

    tast100

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-"""Module multiplies 2 matrices and returns result
+"""Module matrix_mul
+Multiplies two matrices and returns the result.
 """
 
 
 def matrix_mul(m_a, m_b):
-    """Return matrix resulting of
-    the multiplication of m_a and m_b"""
+    """Return the matrix resulting of
+    the multiplication of m_a and m_b."""
 
     if type(m_a) is not list:
-        raiseTypeError("m_a must be a list")
+        raise TypeError("m_a must be a list")
     if type(m_b) is not list:
         raise TypeError("m_b must be a list")
 
@@ -19,7 +20,7 @@ def matrix_mul(m_a, m_b):
         if type(x) is not list:
             raise TypeError("m_b must be a list of lists")
 
-    if m_a == [] or m_a ==[[]]:
+    if m_a == [] or m_a == [[]]:
         raise ValueError("m_a can't be empty")
     if m_b == [] or m_b == [[]]:
         raise ValueError("m_b can't be empty")
@@ -37,12 +38,12 @@ def matrix_mul(m_a, m_b):
     for row in m_a:
         row_len.append(len(row))
     if not all(elem == row_len[0] for elem in row_len):
-        raise TypeError("each row of m_a must should be of the same size")
+            raise TypeError("each row of m_a must should be of the same size")
     row_len = []
     for row in m_b:
         row_len.append(len(row))
     if not all(elem == row_len[0] for elem in row_len):
-        raise TypeError("each row of m_b must should be of the same size")
+            raise TypeError("each row of m_b must should be of the same size")
 
     a_col = 0
     for col in m_a[0]:
