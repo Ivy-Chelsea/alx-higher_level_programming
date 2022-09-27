4a39534b28f1a97a10b9d4a6f26112270dff4928
Ivy-Chelsea <105067998+Ivy-Chelsea@users.noreply.github.com>
2022-09-27 22:04:23 +0300

    fix ambigous variable name

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -17,14 +17,14 @@ def pascal_triangle(n):
     if n <= 0:
         return []
 
-    l = [[0 for x in range(i + 1)] for i in range(n)]
-    l[0] = [1]
+    lst = [[0 for x in range(i + 1)] for i in range(n)]
+    lst[0] = [1]
 
     for i in range(1, n):
-        l[i][0] = 1
+        lst[i][0] = 1
         for j in range(1, i + 1):
-            if j < len(l[i - 1]):
-                l[i][j] = l[i - 1][j - 1] + l[i - 1][j]
+            if j < len(lst[i - 1]):
+                lst[i][j] = lst[i - 1][j - 1] + lst[i - 1][j]
             else:
-                l[i][j] = l[i - 1][0]
-    return l
+                lst[i][j] = lst[i - 1][0]
+    return lst
