e9b622cd0fabc5c5266b46ffa0b795bd7ff05a56
Ivy-Chelsea <ivychelsea20@gmail.com>
2022-11-02 12:54:09 -0700

    task 102

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,7 @@
+ -- Displays 3 cities with highest average temps btwn July & August
+SELECT `city`, AVG(`value`) AS `avg_temp`
+FROM `temperatures`
+WHERE `month` = 7 OR `month` = 8
+GROUP BY `city`
+ORDER BY `avg_temp` DESC
+LIMIT 3;
