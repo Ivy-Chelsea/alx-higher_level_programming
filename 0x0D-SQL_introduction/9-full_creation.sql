63840902ed26cd5fbd85e752df454f915d4d3a48
Ivy-Chelsea <105067998+Ivy-Chelsea@users.noreply.github.com>
2022-11-02 23:32:32 +0300

    change format

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,8 +1,6 @@
 -- Creates and fills a table second_table with attributes id, name and score
-CREATE TABLE IF NOT EXITS `second_table` (`id` INT, `name` VARCHAR(20), `score` INT)
-INSERT INTO `second_table`
-VALUES
-(1, "John" 10);
-(2, "Alex", 3);
-(3, "Bob", 14);
-(4, "George", 8);
+CREATE TABLE IF NOT EXITS `second_table` (`id` INT, `name` VARCHAR(256), `score` INT)
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Alex", 3);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, "Bob", 14);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, "George", 8);
