eebf623a736395fc1be4015fa0567852ffb7ffca
Ivy-Chelsea <ivychelsea20@gmail.com>
2022-11-02 12:10:17 -0700

    task 9

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,8 @@
+-- Creates and fills a table second_table with attributes id, name and score
+CREATE TABLE IF NOT EXITS `second_table` (`id` INT, `name` VARCHAR(20), `score` INT)
+INSERT INTO second_table
+VALUES
+(1, "Scarlet" 10);
+(2, "Keyne", 3);
+(3, "Enoch", 11);
+(4, "Wamalwa", 14);
