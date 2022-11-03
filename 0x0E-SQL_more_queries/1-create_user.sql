7e043a5a7f8f6c01a5b5a05d438537439ad027dd
Ivy-Chelsea <ivychelsea20@gmail.com>
2022-11-03 03:32:18 -0700

    task 1

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,7 @@
+-- Creates the user user_0d_1 with all priviledges
+CREATE USER
+	IF NOT EXISTS 'user_0d_1'@'localhost'
+	IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES
+	ON *.*
+	To 'user_0d_1'@'localhost';
