b003777d230593b3199551b88284e7308a200cb5
Ivy-Chelsea <105067998+Ivy-Chelsea@users.noreply.github.com>
2022-11-03 15:11:54 +0300

    add missing backtick

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -2,6 +2,6 @@
 -- Records are sorted in ascending order of cities.id
 SELECT c.`id`, c.`name`, s.`name`
 	FROM `cities` AS c
-		INNER JOIN `states AS s
+		INNER JOIN `states` AS s
 		ON c.`states_id` = s.`od`
 	ORDER BY c.`id`;
