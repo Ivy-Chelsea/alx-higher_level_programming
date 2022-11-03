331a10f519823ceec0204f21d2bcf472712896cc
Ivy-Chelsea <ivychelsea20@gmail.com>
2022-11-03 03:57:06 -0700

    task 9

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,7 @@
+-- Lists all cities in the database hbtn_0d_usa
+-- Records are sorted in ascending order of cities.id
+SELECT c.`id`, c.`name`, s.`name`
+	FROM `cities` AS c
+		INNER JOIN `states AS s
+		ON c.`states_id` = s.`od`
+	ORDER BY c.`id`;
