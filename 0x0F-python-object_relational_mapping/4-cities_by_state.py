38be29dfc3aeac228edfab031f4544f5cf02269d
Ivy-Chelsea <105067998+Ivy-Chelsea@users.noreply.github.com>
2022-12-03 12:54:56 +0300

    add missing parameter

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -6,7 +6,7 @@ script that lists all cities from database hbtn_0e_4_usa
 import sys
 import MySQLdb
 
-if __name__ == "main__":
+if __name__ == "__main__":
     """results to be ordered in ascending order by cities.id"""
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
