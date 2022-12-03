b61201036e59b63a2bc526e7b96e2e33cf95632f
Ivy-Chelsea <105067998+Ivy-Chelsea@users.noreply.github.com>
2022-12-03 12:36:43 +0300

    task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -3,13 +3,21 @@
 Lists all states with name starting with N from database
 """
 
-import sys
+from sys impory argv
 import MySQLdb
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.curosor()
-    c.execute("SELECT * \
-                FROM `states` \
-                WHERE BINARY `name` = '{}'".format(sys.argv[4]))
-    [print(state) for state in c.fetchall()]
+def get_states(username, password, dbname):
+    """lists all states with a name starting with N from the database"""
+    db = MySQLdb.connect(host="localhost", port=3306, user=str(username),
+                         passwd=str(password), db=str(dbname), charset="utf8")
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states\
+    WHERE name LIKE BINARY '{}'".format(argv[4]))
+    rows = cur.fetchall()
+    for row in rows:
+            print(row)
+    cur.close()
+    db.close()
+
+if __name__ == '__main__':
+        get_states(argv[1], argv[2], argv[3])
