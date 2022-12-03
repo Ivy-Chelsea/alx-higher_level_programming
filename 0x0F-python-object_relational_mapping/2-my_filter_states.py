f866674457d162eea6bb780d30993988f39b5a29
Ivy-Chelsea <105067998+Ivy-Chelsea@users.noreply.github.com>
2022-12-03 12:43:00 +0300

    add/remove whitespaces

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -6,6 +6,7 @@ Lists all states with name starting with N from database
 import MySQLdb
 from sys import argv
 
+
 def get_states(username, password, dbname):
     """lists all states with a name starting with N from the database"""
     db = MySQLdb.connect(host="localhost", port=3306, user=str(username),
@@ -15,9 +16,10 @@ def get_states(username, password, dbname):
     WHERE name LIKE BINARY '{}'".format(argv[4]))
     rows = cur.fetchall()
     for row in rows:
-            print(row)
+        print(row)
     cur.close()
     db.close()
 
+
 if __name__ == '__main__':
-        get_states(argv[1], argv[2], argv[3])
+    get_states(argv[1], argv[2], argv[3])
