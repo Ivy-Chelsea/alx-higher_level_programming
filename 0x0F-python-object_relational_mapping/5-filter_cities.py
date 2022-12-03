3413cae38e7734f10e41fc392b6b8ec0676c1719
Ivy-Chelsea <105067998+Ivy-Chelsea@users.noreply.github.com>
2022-12-03 12:58:29 +0300

    comment

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
+"""
+Script that takes name of state as argument and lists all cities in that state
+"""
 
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
+    """script should take 4 arguments"""
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
     c.execute("SELECT * FROM `cities` as `c` \
