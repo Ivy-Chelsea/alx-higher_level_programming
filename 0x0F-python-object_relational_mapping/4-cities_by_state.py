ccf52aa4ede28bf5bc0e22a6e75e27fa9d546d29
Ivy-Chelsea <105067998+Ivy-Chelsea@users.noreply.github.com>
2022-12-03 12:49:24 +0300

    comment

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
+"""
+script that lists all cities from database hbtn_0e_4_usa
+"""
 
 import sys
 import MySQLdb
 
 if __name__ == "main__":
+    """results to be ordered in ascending order by cities.id"""
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
     c.execute("SELECT `c`.`id`, `c`.`name`, `s`.`name` \
@@ -12,4 +16,3 @@ if __name__ == "main__":
                 ON `c`.`state_id` = `s`.`id` \
             ORDER BY `c`.`id`")
     [print(city) for city in c.fetchall()]
-
