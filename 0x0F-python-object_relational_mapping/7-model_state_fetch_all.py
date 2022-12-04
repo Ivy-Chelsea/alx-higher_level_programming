a09b7dc16d26f07866fd1ceffa28a2fc1e26aca7
Ivy-Chelsea <105067998+Ivy-Chelsea@users.noreply.github.com>
2022-12-04 23:25:16 +0300

    remove unnecessary whitespaces

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -11,8 +11,8 @@ from model_state import State
 if __name__ == "__main__":
     """Class inherits from Base"""
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                            pool_pre_ping=True)
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = SEssion()
 
