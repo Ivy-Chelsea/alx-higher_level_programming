556dc86791b883df99dcec9bd12d957d3fe3f91e
Ivy-Chelsea <105067998+Ivy-Chelsea@users.noreply.github.com>
2022-12-04 23:27:17 +0300

    change uppercase to lowercase

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
-    session = SEssion()
+    session = Session()
 
     for state in session.query(State).order_by(State.id):
         print("{}: {}".format(state.id, state.name))
