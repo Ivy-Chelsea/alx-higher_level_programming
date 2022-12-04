f6ec01b0cd4279f0217ccad3a4778a237a0be598
Ivy-Chelsea <105067998+Ivy-Chelsea@users.noreply.github.com>
2022-12-04 23:32:59 +0300

    remove unnecessary whitespacaes

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-
+"""
+Script that lists all State objects that contain letter a from database
+"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -7,8 +9,8 @@ from model_state import State
 
 if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                             pool_pre_ping=True)
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
 
