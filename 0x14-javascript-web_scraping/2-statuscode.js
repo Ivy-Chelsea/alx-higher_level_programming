e8a55b517db8136fe7b4687f4de38ca346927de6
Ivy-Chelsea <105067998+Ivy-Chelsea@users.noreply.github.com>
2023-01-18 00:21:10 +0300

    task 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,9 +4,9 @@ const request = require('request');
 const URL = process.argv[2];
 
 request(URL, function (err, response) {
-	if (err) {
-		console.log(err);
-	} else {
-		console.log('code: ' + response.statusCode);
-	}
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('code: ' + response.statusCode);
+  }
 });
