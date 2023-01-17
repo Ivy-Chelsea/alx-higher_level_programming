d68e0534fc10e1ad206f3567d5fe485b2334aeb2
Ivy-Chelsea <105067998+Ivy-Chelsea@users.noreply.github.com>
2023-01-18 00:26:35 +0300

    task 5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -3,7 +3,7 @@ const request = require('request');
 const fs = require('fs');
 
 request(process.argv[2], function (err, response, body) {
-	if (err == null) {
-		fs.writeFileSync(process.argv[3], body);
-	}
+  if (err == null) {
+    fs.writeFileSync(process.argv[3], body);
+  }
 });
