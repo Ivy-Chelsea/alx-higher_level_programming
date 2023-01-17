690e096a7570fdff3a49e26456f3d7396ba98fdd
Ivy-Chelsea <105067998+Ivy-Chelsea@users.noreply.github.com>
2023-01-18 00:18:30 +0300

    task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,9 +4,9 @@ const fs = require('fs');
 const file = process.argv[2];
 
 fs.readFile(file, 'utf-8', function (err, data) {
-	if (err) {
-		console.log(err);
-	} else {
-		console.log(data);
-	}
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
 });
